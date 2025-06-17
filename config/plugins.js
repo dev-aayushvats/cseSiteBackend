c021b99afbbb8cb7371e60bc7707f98a0442fb74
swasti-sj <jainswasti2004@gmail.com>
2025-06-17 22:38:49 +0530

    activating fuzzy seach for research page

--- config/plugins.js
+++ config/plugins.js
@@ -16,6 +16,39 @@ module.exports = () => ({
             ],
           },
         },
+        {
+  uid: 'api::research-lab.research-lab',
+  modelName: 'research-lab',
+  fuzzysortOptions: {
+    characterLimit: 300,
+    threshold: 0.6,
+    limit: 10,
+    keys: [
+      { name: 'Name', weight: 0.3 },
+      { name: 'Type', weight: 0.1 },
+       { name: 'Link', weight: 0.1 }
+    ],
+  },
+},
+{
+  uid: 'api::research-project.research-project',
+  modelName: 'research-project',
+  fuzzysortOptions: {
+    characterLimit: 300,
+    threshold: 0.6,
+    limit: 10,
+    keys: [
+      { name: 'Title', weight: 0.3 },
+      { name: 'Area', weight: 0.1 },
+      { name: 'PI', weight: 0.1 },
+        { name: 'Type', weight: 0.1 },
+          { name: 'Duration', weight: 0.1 },
+            { name: 'CoPI', weight: 0.1 },
+               { name: 'CurrentStatus', weight: 0.1 },
+    ],
+  },
+},
+
 	{
           uid: 'api::about-page.about-page',
           modelName: 'about-page',
